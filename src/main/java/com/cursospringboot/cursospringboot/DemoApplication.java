3101d8c0c33a945cd0fa8c21495117143815951a
paulo henrique <p.henriquerm@hotmail.com>
2020-08-14 17:12:46 -0300

    Criando operação de instanciação

--- src/main/java/com/cursospringboot/cursospringboot/DemoApplication.java
+++ src/main/java/com/cursospringboot/cursospringboot/DemoApplication.java
@@ -1,13 +1,33 @@
 package com.cursospringboot.cursospringboot;
 
+import java.util.Arrays;
+
+import com.cursospringboot.cursospringboot.domain.Categoria;
+import com.cursospringboot.cursospringboot.repositories.CategoriaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication  implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
 	}
 
 }
