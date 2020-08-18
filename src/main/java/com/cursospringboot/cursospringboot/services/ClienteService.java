31d6b203471f9ca13e551d76ae17ca9bfa21781a
paulo henrique <p.henriquerm@hotmail.com>
2020-08-18 15:05:03 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/cursospringboot/cursospringboot/services/ClienteService.java
@@ -0,0 +1,27 @@
+package com.cursospringboot.cursospringboot.services;
+
+import java.util.Optional;
+
+import com.cursospringboot.cursospringboot.domain.Cliente;
+import com.cursospringboot.cursospringboot.repositories.ClienteRepository;
+import com.cursospringboot.cursospringboot.services.exceptions.ObjectNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository repo;
+
+    public Cliente buscar(Integer id) {
+        Optional<Cliente> obj = repo.findById(id);
+        return obj.orElseThrow(
+            () -> 
+                new ObjectNotFoundException(
+                    "Objeto não encontrado! Id:" + id + ", Tipo: " + Cliente.class.getName()
+                )
+            );
+    }
+}
