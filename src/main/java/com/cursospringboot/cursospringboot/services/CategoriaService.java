ba135ca71957db2fd0fcb33636f1b50636739d2d
paulo henrique <paulomorais@great.ufc.br>
2020-08-20 15:19:02 -0300

    Inserindo uma nova Categoria com post

--- src/main/java/com/cursospringboot/cursospringboot/services/CategoriaService.java
+++ src/main/java/com/cursospringboot/cursospringboot/services/CategoriaService.java
@@ -24,4 +24,9 @@ public class CategoriaService {
                 )
             );
     }
+
+    public Categoria insert(Categoria obj){
+        obj.setId(null);
+        return repo.save(obj);
+    }
 }
