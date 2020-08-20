1da2ff2d982723c12cdb9a16dae64995c9eca99a
paulo henrique <paulomorais@great.ufc.br>
2020-08-20 17:16:53 -0300

    Paginação com paramentros opcionais na requisição

--- src/main/java/com/cursospringboot/cursospringboot/resources/CategoriaResource.java
+++ src/main/java/com/cursospringboot/cursospringboot/resources/CategoriaResource.java
@@ -5,6 +5,7 @@ import com.cursospringboot.cursospringboot.dto.CategoriaDTO;
 import com.cursospringboot.cursospringboot.services.CategoriaService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -62,6 +63,20 @@ public class CategoriaResource {
                     .stream()
                         .map(obj -> new CategoriaDTO(obj))
                         .collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(listDto);
+    }
+
+    @RequestMapping(value = "/page",method = RequestMethod.GET)
+    public ResponseEntity<Page<CategoriaDTO>> findPage(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "24")Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "nome")String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC")String direction)
+    {
+        Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+        Page<CategoriaDTO> listDto =
+                list.map(obj -> new CategoriaDTO(obj));
 
         return ResponseEntity.ok().body(listDto);
     }
--- src/main/java/com/cursospringboot/cursospringboot/services/CategoriaService.java
+++ src/main/java/com/cursospringboot/cursospringboot/services/CategoriaService.java
@@ -10,6 +10,9 @@ import com.cursospringboot.cursospringboot.services.exceptions.ObjectNotFoundExc
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -49,5 +52,10 @@ public class CategoriaService {
 
     public List<Categoria> findAll(){
         return repo.findAll();
+    }
+
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return repo.findAll(pageRequest);
     }
 }
