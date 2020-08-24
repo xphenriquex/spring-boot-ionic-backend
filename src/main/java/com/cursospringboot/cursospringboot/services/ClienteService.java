d922b88b1a4bf40fe4ba185ae92fd6171bd604b8
paulo henrique <paulomorais@great.ufc.br>
2020-08-24 14:05:25 -0300

    Ajustando e testando a deleção de cliente

--- src/main/java/com/cursospringboot/cursospringboot/services/ClienteService.java
+++ src/main/java/com/cursospringboot/cursospringboot/services/ClienteService.java
@@ -59,7 +59,7 @@ public class ClienteService {
         try {
             repo.deleteById(id);
         }catch (DataIntegrityViolationException e){
-            throw new DataIntegrityException("Não é possível excluir há entidades relacionadas");
+            throw new DataIntegrityException("Não é possível excluir há pedidos relacionadas");
         }
     }
 
