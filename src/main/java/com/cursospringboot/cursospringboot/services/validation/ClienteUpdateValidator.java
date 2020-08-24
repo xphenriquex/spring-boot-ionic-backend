27058fa63fdf6f2e3363b1b8fca69669c085d890
paulo henrique <paulomorais@great.ufc.br>
2020-08-24 15:41:25 -0300

    Validação customizada: email não repetido na atualização de cliente

---
+++ src/main/java/com/cursospringboot/cursospringboot/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,53 @@
+package com.cursospringboot.cursospringboot.services.validation;
+
+import com.cursospringboot.cursospringboot.domain.Cliente;
+import com.cursospringboot.cursospringboot.domain.enums.TipoCliente;
+import com.cursospringboot.cursospringboot.dto.ClienteDTO;
+import com.cursospringboot.cursospringboot.dto.ClienteNewDTO;
+import com.cursospringboot.cursospringboot.repositories.ClienteRepository;
+import com.cursospringboot.cursospringboot.resources.exceptions.FieldMessage;
+import com.cursospringboot.cursospringboot.services.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private ClienteRepository repo;
+
+    @Override
+    public void initialize(ClienteUpdate ann) {
+    }
+
+    @Override
+    public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+        @SuppressWarnings("unchecked")
+        Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        Integer uriId = Integer.parseInt(map.get("id"));
+
+        List<FieldMessage> list = new ArrayList<>();
+
+        Cliente aux = repo.findByEmail(objDto.getEmail());
+
+        if(aux != null && !aux.getId().equals(uriId)){
+            list.add(new FieldMessage("email", "Email já existente"));
+        }
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage())
+                    .addPropertyNode(e.getFieldName()).addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
