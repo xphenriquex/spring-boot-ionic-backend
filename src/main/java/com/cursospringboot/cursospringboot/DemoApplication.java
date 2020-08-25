8e6eb23e0d01162e24f6add64f49e707d5f84581
paulo henrique <paulomorais@great.ufc.br>
2020-08-25 14:07:18 -0300

    Criando profile de desenvolvimento

--- src/main/java/com/cursospringboot/cursospringboot/DemoApplication.java
+++ src/main/java/com/cursospringboot/cursospringboot/DemoApplication.java
@@ -1,32 +1,5 @@
 package com.cursospringboot.cursospringboot;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
-import com.cursospringboot.cursospringboot.domain.Categoria;
-import com.cursospringboot.cursospringboot.domain.Cidade;
-import com.cursospringboot.cursospringboot.domain.Cliente;
-import com.cursospringboot.cursospringboot.domain.Endereco;
-import com.cursospringboot.cursospringboot.domain.Estado;
-import com.cursospringboot.cursospringboot.domain.ItemPedido;
-import com.cursospringboot.cursospringboot.domain.Pagamento;
-import com.cursospringboot.cursospringboot.domain.PagamentoComBoleto;
-import com.cursospringboot.cursospringboot.domain.PagamentoComCartao;
-import com.cursospringboot.cursospringboot.domain.Pedido;
-import com.cursospringboot.cursospringboot.domain.Produto;
-import com.cursospringboot.cursospringboot.domain.enums.EstadoPagamento;
-import com.cursospringboot.cursospringboot.domain.enums.TipoCliente;
-import com.cursospringboot.cursospringboot.repositories.CategoriaRepository;
-import com.cursospringboot.cursospringboot.repositories.CidadeRepository;
-import com.cursospringboot.cursospringboot.repositories.ClienteRepository;
-import com.cursospringboot.cursospringboot.repositories.EnderecoRepository;
-import com.cursospringboot.cursospringboot.repositories.EstadoRepository;
-import com.cursospringboot.cursospringboot.repositories.ItemPedidoRepository;
-import com.cursospringboot.cursospringboot.repositories.PagamentoRepository;
-import com.cursospringboot.cursospringboot.repositories.PedidoRepository;
-import com.cursospringboot.cursospringboot.repositories.ProdutoRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
