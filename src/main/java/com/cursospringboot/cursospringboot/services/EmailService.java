18b06c57137762236ff5af41cebd021acf67c089
paulo henrique <paulomorais@great.ufc.br>
2020-08-26 12:19:20 -0300

    Email HTML

--- src/main/java/com/cursospringboot/cursospringboot/services/EmailService.java
+++ src/main/java/com/cursospringboot/cursospringboot/services/EmailService.java
@@ -3,9 +3,15 @@ package com.cursospringboot.cursospringboot.services;
 import com.cursospringboot.cursospringboot.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 
 public interface EmailService {
     void sendOrderConfirmationEmail(Pedido obj);
 
     void sendEmail(SimpleMailMessage msg);
+
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+    void sendHtmlEmail(MimeMessage msg);
 }
--- src/main/java/com/cursospringboot/cursospringboot/services/SmtpEmailService.java
+++ src/main/java/com/cursospringboot/cursospringboot/services/SmtpEmailService.java
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import javax.mail.internet.MimeMessage;
 
 public class SmtpEmailService extends AbstractEmailService {
     private  static final Logger LOG = LoggerFactory.getLogger(EmailService.class);
@@ -12,10 +15,20 @@ public class SmtpEmailService extends AbstractEmailService {
     @Autowired
     private MailSender mailSender;
 
+    @Autowired
+    private JavaMailSender javaMailSender;
+
     @Override
     public void sendEmail(SimpleMailMessage msg) {
         LOG.info("enviando email...");
         mailSender.send(msg);
+        LOG.info("Email enviado");
+    }
+
+    @Override
+    public void sendHtmlEmail(MimeMessage msg) {
+        LOG.info("enviando email...");
+        javaMailSender.send(msg);
         LOG.info("Email enviado");
     }
 }
