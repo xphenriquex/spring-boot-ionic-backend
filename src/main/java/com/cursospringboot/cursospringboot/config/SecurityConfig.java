c0914afca2afb637647d0564c23ece55f7920a71
paulo henrique <paulomorais@great.ufc.br>
2020-08-27 14:17:16 -0300

    Implementando autenticação e geração do token JWT e consertando Bearer escrito incorreto em JWTAuthenticationFilter

--- src/main/java/com/cursospringboot/cursospringboot/config/SecurityConfig.java
+++ src/main/java/com/cursospringboot/cursospringboot/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.cursospringboot.cursospringboot.config;
 
 import com.cursospringboot.cursospringboot.security.JWTAuthenticationFilter;
+import com.cursospringboot.cursospringboot.security.JWTAuthorizationFilter;
 import com.cursospringboot.cursospringboot.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -57,6 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated();
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
--- src/main/java/com/cursospringboot/cursospringboot/security/JWTAuthenticationFilter.java
+++ src/main/java/com/cursospringboot/cursospringboot/security/JWTAuthenticationFilter.java
@@ -61,7 +61,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     {
         String username = ((UserSS) authResult.getPrincipal()).getUsername();
         String token = jwtUtil.generateToken(username);
-        response.addHeader("Authorization", "Beare" + token);
+        response.addHeader("Authorization", "Bearer " + token);
     }
 
 
