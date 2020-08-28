4261582b880fdc93f0d5a52ba0d3d8fd3eb6c686
paulo henrique <paulomorais@great.ufc.br>
2020-08-28 14:36:51 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/cursospringboot/cursospringboot/DemoApplication.java
+++ src/main/java/com/cursospringboot/cursospringboot/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.cursospringboot.cursospringboot;
 
+import com.cursospringboot.cursospringboot.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,13 +9,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication  implements CommandLineRunner{
 
+	@Autowired
+	private S3Service s3Service;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		s3Service.uploadFile("C:\\projects\\fotos\\filhotes.jpg");
 	}
 
 }
