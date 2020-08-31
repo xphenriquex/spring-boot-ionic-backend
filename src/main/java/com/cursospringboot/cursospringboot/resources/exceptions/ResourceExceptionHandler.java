d24ab4f78632b18a03e933445dab31f6df866cd4
paulo henrique <paulomorais@great.ufc.br>
2020-08-31 08:43:20 -0300

    Tratando exceções adequadamente

--- src/main/java/com/cursospringboot/cursospringboot/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/cursospringboot/cursospringboot/resources/exceptions/ResourceExceptionHandler.java
@@ -2,8 +2,12 @@ package com.cursospringboot.cursospringboot.resources.exceptions;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.cursospringboot.cursospringboot.services.exceptions.AuthorizationException;
 import com.cursospringboot.cursospringboot.services.exceptions.DataIntegrityException;
+import com.cursospringboot.cursospringboot.services.exceptions.FileException;
 import com.cursospringboot.cursospringboot.services.exceptions.ObjectNotFoundException;
 
 import org.springframework.http.HttpStatus;
@@ -43,5 +47,31 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> authorization(AuthorizationException e, HttpServletRequest request){
         StandardError err = new StandardError(HttpStatus.FORBIDDEN.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
+    }
+
+    @ExceptionHandler(FileException.class)
+    public ResponseEntity<StandardError> file(FileException e, HttpServletRequest request){
+        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+
+    @ExceptionHandler(AmazonServiceException.class)
+    public ResponseEntity<StandardError> amazonService(AmazonServiceException e, HttpServletRequest request){
+
+        HttpStatus code = HttpStatus.valueOf(e.getErrorCode());
+        StandardError err = new StandardError(code.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(code).body(err);
+    }
+
+    @ExceptionHandler(AmazonClientException.class)
+    public ResponseEntity<StandardError> amazonClient(AmazonClientException e, HttpServletRequest request){
+        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+
+    @ExceptionHandler(AmazonS3Exception.class)
+    public ResponseEntity<StandardError> amazonS3(AmazonS3Exception e, HttpServletRequest request){
+        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }
--- src/main/java/com/cursospringboot/cursospringboot/services/S3Service.java
+++ src/main/java/com/cursospringboot/cursospringboot/services/S3Service.java
@@ -5,6 +5,7 @@ import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.cursospringboot.cursospringboot.services.exceptions.FileException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,7 +36,7 @@ public class S3Service {
             String contentType = multipartFile.getContentType();
             return uploadFile(is, fileName, contentType);
         } catch (IOException e) {
-            throw new RuntimeException("Erro DE io: " + e.getMessage());
+            throw new FileException("Erro DE io: " + e.getMessage());
         }
     }
 
@@ -48,7 +49,7 @@ public class S3Service {
             LOG.info("Upload finalizado");
             return s3cliente.getUrl(bucketName, fileName).toURI();
         } catch (URISyntaxException e) {
-           throw new RuntimeException("Erro ao converter URL para URI");
+           throw new FileException("Erro ao converter URL para URI");
         }
     }
 }
